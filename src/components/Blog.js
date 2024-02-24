4ea787bdf1f1bbd8e92b73128271c36680240667
subashree3456 <107339951+subashree3456@users.noreply.github.com>
2024-02-24 18:18:44 +0530

    Add files via upload

---
+++ src/components/Blog.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import {
+    Avatar,
+    Box,
+    Card,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    IconButton,
+    Typography,
+  } from "@mui/material";
+  import axios from "axios";
+  import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useNavigate } from "react-router-dom";
+
+const Blog = ({ title, description, imageURL, userName, isUser, id}) => {
+  const navigate = useNavigate();
+  const handleEdit = () => {
+    navigate(`/myBlogs/${id}`);
+  };
+
+  const deleteRequest = async () => {
+    const res = await axios
+      .delete(`https://blog-backend-1-j1jt.onrender.com/api/blog/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+
+  const handleDelete = () => {
+    deleteRequest()
+    .then(() => navigate("/"))
+    .then(() => navigate("/blogs"));
+    
+  };
+
+
+
+  return (
+    <div>
+          {" "}
+ <Card sx={{
+          width: "40%",
+          margin: "auto",
+          mt: 2,
+          padding: 2,
+          boxShadow: "5px 5px 10px #ccc",
+          ":hover": {
+            boxShadow: "10px 10px 20px #ccc",
+          },
+        }} >
+
+{isUser && (
+          <Box display="flex">
+            <IconButton  onClick={handleEdit}  sx={{ marginLeft: "auto" }}>
+              <ModeEditOutlineIcon color="warning" />
+            </IconButton>
+            <IconButton  onClick={handleDelete}>
+              <DeleteForeverIcon color="error" />
+            </IconButton>
+          </Box>
+        )}
+
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+           {userName}
+          </Avatar>
+        }
+        
+        title={title}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={imageURL}
+        alt="Paella dish"
+      />
+
+
+
+      <CardContent>
+      <hr/>
+      <br />
+        <Typography variant="body2" color="text.secondary">
+        <b>{userName}</b> {": "} {description}
+        </Typography>
+      </CardContent>
+      
+    </Card>
+
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/components/Blogs.js
@@ -0,0 +1,43 @@
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Blog from "./Blog";
+const Blogs = () => {
+
+  const [blogs, setBlogs] = useState();
+
+  const sendRequest = async () => {
+    const res = await axios
+      .get("https://blog-backend-1-j1jt.onrender.com/api/blog")
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+
+
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
+
+  console.log(blogs);
+
+  return (
+    <div>  
+     
+      {blogs &&
+        blogs.map((blog, index) => (
+          <Blog
+          id={blog._id}
+          isUser={localStorage.getItem("userId") === blog.user._id}
+          title={blog.title}
+          description={blog.description}
+          imageURL={blog.image}
+          userName={blog.user.name}
+          />
+        ))}
+
+    </div>
+  )
+}
+
+export default Blogs
